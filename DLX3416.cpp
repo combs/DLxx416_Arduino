07256e1b22987ff06234f15137cf5a70bc159340
Chris Combs <ccombs@gmail.com>
2020-11-06 18:10:06 -0500

    not yet working... but closer

--- DLX3416.cpp
+++ DLX3416.cpp
@@ -3,17 +3,19 @@
 #include <stdint.h>
 #include "Print.h"
 
-#include "DLX2416.h"
+#include "DLX3416.h"
 
-DLX2416::DLX2416(uint8_t pinA0, uint8_t pinA1, uint8_t pinBL_AL, uint8_t pinCE1_AL, uint8_t pinCE2_AL, uint8_t pinCLR_AL, uint8_t pinCU_AL, uint8_t pinCUE, uint8_t pinD0, uint8_t pinD1, uint8_t pinD2, uint8_t pinD3, uint8_t pinD4, uint8_t pinD5, uint8_t pinD6, uint8_t pinWR_AL, uint8_t usePortD) {
+DLX3416::DLX3416(uint8_t pinA0, uint8_t pinA1, uint8_t pinBL_AL, uint8_t pinCE1, uint8_t pinCE2, uint8_t pinCE3_AL, uint8_t pinCE4_AL, uint8_t pinCLR_AL, uint8_t pinCU, uint8_t pinCUE, uint8_t pinD0, uint8_t pinD1, uint8_t pinD2, uint8_t pinD3, uint8_t pinD4, uint8_t pinD5, uint8_t pinD6, uint8_t pinWR_AL, uint8_t usePortD) {
 
     PIN_ADDRESS0 = pinA0;
     PIN_ADDRESS1 = pinA1;
     PIN_BL_AL = pinBL_AL;
-    PIN_CE1_AL = pinCE1_AL; // null val in my application
-    PIN_CE2_AL = pinCE2_AL;
+    PIN_CE1 = pinCE1; 
+    PIN_CE2 = pinCE2;
+    PIN_CE3_AL = pinCE3_AL; 
+    PIN_CE4_AL = pinCE4_AL;
     PIN_CLR_AL = pinCLR_AL;
-    PIN_CU_AL = pinCU_AL;
+    PIN_CU = pinCU;
     PIN_CUE = pinCUE;
     PIN_D0 = pinD0;
     PIN_D1 = pinD1;
@@ -27,14 +29,16 @@ DLX2416::DLX2416(uint8_t pinA0, uint8_t pinA1, uint8_t pinBL_AL, uint8_t pinCE1_
     
 }
 
-void DLX2416::begin() {
+void DLX3416::begin() {
     pinMode(PIN_ADDRESS0, OUTPUT);
     pinMode(PIN_ADDRESS1, OUTPUT);
     pinMode(PIN_BL_AL, OUTPUT);
-    pinMode(PIN_CE1_AL, OUTPUT);
-    pinMode(PIN_CE2_AL, OUTPUT);
+    pinMode(PIN_CE1, OUTPUT);
+    pinMode(PIN_CE2, OUTPUT);
+    pinMode(PIN_CE3_AL, OUTPUT);
+    pinMode(PIN_CE4_AL, OUTPUT);
     pinMode(PIN_CLR_AL, OUTPUT);
-    pinMode(PIN_CU_AL, OUTPUT);
+    pinMode(PIN_CU, OUTPUT);
     pinMode(PIN_CUE, OUTPUT);
     pinMode(PIN_WR_AL, OUTPUT);
 
@@ -52,26 +56,36 @@ void DLX2416::begin() {
         pinMode(PIN_D6, OUTPUT);
     }
     digitalWrite(PIN_BL_AL, HIGH);
-    digitalWrite(PIN_CE1_AL, HIGH);
-    digitalWrite(PIN_CE2_AL, HIGH);
+    digitalWrite(PIN_CE1, LOW);
+    digitalWrite(PIN_CE2, LOW);
+    digitalWrite(PIN_CE3_AL, HIGH);
+    digitalWrite(PIN_CE4_AL, HIGH);
     digitalWrite(PIN_CLR_AL, HIGH);
-    digitalWrite(PIN_CU_AL, HIGH);
+    digitalWrite(PIN_CU, LOW);
     digitalWrite(PIN_CUE, LOW);
     digitalWrite(PIN_WR_AL, HIGH);
             
 }
-void DLX2416::set_portd(uint8_t theByte) {
+void DLX3416::set_portd(uint8_t theByte) {
     uint8_t bit7 = PORTD & B10000000; // save current bit 7
     theByte = theByte & B01111111; // wipe bit 7 in new val
     PORTD = theByte | bit7; // write new vals
 }
 
-void DLX2416::writeByte(uint8_t display, uint8_t address, uint8_t theByte) {
+void DLX3416::writeByte(uint8_t display, uint8_t address, uint8_t theByte) {
     digitalWrite(PIN_ADDRESS0, address & B00000001);
     digitalWrite(PIN_ADDRESS1, address & B00000010);
 
+    digitalWrite(PIN_CE1, (display & B00000001) );  // active high 
+    digitalWrite(PIN_CE2, (display & B00000010) ); // active high 
+    digitalWrite(PIN_CE3_AL, (display & B00000001) == 0);  // active low so inverted
+    digitalWrite(PIN_CE4_AL, (display & B00000010) == 0); // active low so inverted
+    
     delayMicroseconds(2);
 
+    digitalWrite(PIN_WR_AL, LOW);
+    delayMicroseconds(2);
+    
     if (D_USE_PORTD) {
         set_portd(theByte);
     } else {
@@ -86,21 +100,17 @@ void DLX2416::writeByte(uint8_t display, uint8_t address, uint8_t theByte) {
     }
     
     delayMicroseconds(2);
-
-    digitalWrite(PIN_CE1_AL, (display & B00000001) == 0);  // active low so inverted
-    digitalWrite(PIN_CE2_AL, (display & B00000010) == 0); // active low so inverted
-    
-    digitalWrite(PIN_WR_AL, LOW);
-    delayMicroseconds(2);
     
     digitalWrite(PIN_WR_AL, HIGH);
     delayMicroseconds(2);
 
-    digitalWrite(PIN_CE1_AL, HIGH);
-    digitalWrite(PIN_CE2_AL, HIGH);
+    digitalWrite(PIN_CE1, LOW);
+    digitalWrite(PIN_CE2, LOW);
+    digitalWrite(PIN_CE3_AL, HIGH);
+    digitalWrite(PIN_CE4_AL, HIGH);
 
 }
-void DLX2416::clear() {
+void DLX3416::clear() {
     digitalWrite(PIN_CLR_AL, LOW);
     delayMicroseconds(1);
     digitalWrite(PIN_CLR_AL, HIGH);
